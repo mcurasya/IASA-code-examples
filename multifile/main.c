0400e4f768a39e008e22934869f49dfad59b13bc
yura <mcurasya@gmail.com>
2020-03-05 09:06:19 +0200

    back to extern

--- multifile/main.c
+++ multifile/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include "dependency2.c"
-#include "dependency1.c"
-//extern int func1();
 
-//extern void func2();
+extern int func1();
+
+extern void func2();
 
 int main(){
     printf("%d\n",func1());

4b9d4ca30d4245e59c05dc9324825defe85971d9
mcurasya <vorobyov.h@gmail.com>
2020-03-21 21:00:59 +0200

    added build for opengl for macos and linux

--- opengl/test/build.sh
+++ opengl/test/build.sh
@@ -1,1 +1,5 @@
-g++ main.cpp -o firstOpenGlApp -lglut -lGLU -lGL -lm
+if [ "$(uname)" == "Darwin" ]; then
+    g++ main.cpp -framework GLUT -framework OpenGL -Wno-deprecated -o result
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    g++ main.cpp -lglut -lGLU -lGL -lm -o result
+fi 

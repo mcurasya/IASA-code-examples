04bd89c5c5d1b9caa90c8445485fd5fa602c2cbb
yura <mcurasya@gmail.com>
2020-03-22 23:42:00 +0200

    started binary

--- binary/laba14.cpp
+++ binary/laba14.cpp
@@ -2,6 +2,7 @@
 #include "structs.hpp"
 
 using namespace std;
+using namespace structs;
 
 void add_record();
 void show_records();
@@ -21,7 +22,7 @@ int main(int argc, const char **argv)
              << "3 - change record\n"
              << "4 - delete record\n"
              << "5 - show best faculty\n"
-        	 << "6 - show best student\n"
+             << "6 - show best student\n"
              << "7 - show groups without bad students\n"
              << "8 - exit\n"
              << ">> ";
@@ -29,18 +30,25 @@ int main(int argc, const char **argv)
         switch (pick)
         {
         case 1:
+            add_record();
             break;
         case 2:
+            show_records();
             break;
         case 3:
+        change_record();
             break;
         case 4:
+        delete_record();
             break;
         case 5:
+        best_faculty();
             break;
         case 6:
+        best_student();
             break;
         case 7:
+        good_groups();
             break;
         case 8:
             exit(0);
@@ -52,7 +60,13 @@ int main(int argc, const char **argv)
     return 0;
 }
 
-void add_record(){
+void add_record()
+{
+    cout << "input last name->";
+    char* name = (char*)calloc(15, sizeof(char));
+    scanf("%s", name);
 }
 
-void show_records(){}
+void show_records() {
+
+}

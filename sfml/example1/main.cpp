ef8a8043c9fd3a6949dc493b8c4c076ff59884ef
yura <mcurasya@gmail.com>
2020-04-22 02:40:43 +0300

    example1

--- sfml/example1/main.cpp
+++ sfml/example1/main.cpp
@@ -3,8 +3,14 @@
 int main(int argc, char const *argv[])
 {
     //initialise
+    float rotationspeed = 0.f;
     sf::RenderWindow window(sf::VideoMode(1200, 900), "example 1");
-
+    sf::RectangleShape square(sf::Vector2f(250, 250));
+    square.setFillColor(sf::Color::Cyan);
+    square.setOutlineColor(sf::Color::Green);
+    square.setOutlineThickness(5);
+    square.setOrigin(square.getLocalBounds().width / 2, square.getLocalBounds().height / 2);
+    square.setPosition(window.getSize().x / 2, window.getSize().y / 2);
     //main loop
     while (window.isOpen())
     {
@@ -12,17 +18,27 @@ int main(int argc, char const *argv[])
         //event loop
         while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if(event.key.code == sf::Keyboard::Up && rotationspeed < 1.f){
+                    rotationspeed+=0.01;
+                }
+                if(event.key.code == sf::Keyboard::Down && rotationspeed > -1.f){
+                    rotationspeed-=0.01;
+                }
+            }
         }
-        //update 
-
+        //update
+        square.rotate(rotationspeed);
         //redraw
         window.clear();
+        window.draw(square);
         window.display();
     }
-    
+
     return 0;
 }
